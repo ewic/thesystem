7803e230d8c6243fd146ccb1415a539e7900dff5
Eric Zhang <ezhang@insuremytrip.com>
2014-09-27 00:50:30 -0400

    First user migrated in, added to app_admin

--- app/database/migrations/2014_09_26_235128_first_user.php
+++ app/database/migrations/2014_09_26_235128_first_user.php
@@ -13,7 +13,22 @@ class FirstUser extends Migration {
 	public function up()
 	{
 		//Insert the first user, a super admin.  Also create the super admin table
-		
+		DB::table('users')->insertGetId(
+			array(	'email' => 'ewicexclamationpoint@gmail.com',
+					'password' => MD5('test'),
+					'name' => 'Eric Zhang',
+					'emplid' => 100194778,)
+			);
+
+		Schema::create('app_admin',function($table) {
+			$table->integer('id');
+		});
+
+		$id = DB::table('users')->where('email','=','ewicexclamationpoint@gmail.com')->pluck('id');
+
+		DB::table('app_admin')->insert(
+			array('id' => $id)
+			);
 	}
 
 	/**
@@ -24,6 +39,12 @@ class FirstUser extends Migration {
 	public function down()
 	{
 		//Drop the first user
+		$id = DB::table('users')->where('email','=','ewicexclamationpoint@gmail.com')->pluck('id');
+
+		DB::table('app_admin')->where('id','=',$id)->delete();
+
+		DB::table('users')->where('email','=','ewicexclamationpoint@gmail.com')->delete();
+		
 	}
 
 }
